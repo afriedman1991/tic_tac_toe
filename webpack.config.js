8cd5db749eeade6d4ca25beb3c209fea9faed84e
afriedman1991@gmail.com <afriedman1991@gmail.com>
2018-03-13 15:44:52 -0400

    Set up webpack config file with jsx loaders

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/public/dist');
+
+module.exports = {
+    entry: `${SRC_DIR}/index.jsx`,
+    output: {
+        filename: 'bundle.js',
+        path: DIST_DIR
+    },
+    loaders: [
+        {
+            test: /\.jsx?/,
+            include: SRC_DIR,
+            loader:  'babel-loader',
+            query: {
+                presets: ['react', 'es2015']
+            }
+        }
+    ]
+}
