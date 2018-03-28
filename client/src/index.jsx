12c665b9584c02de94d070fb8c45b6b9668d87c4
afriedman1991@gmail.com <afriedman1991@gmail.com>
2018-03-28 15:57:29 -0400

    Updated index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import ReactDOM, { render } from 'react-dom';
 
-class Board extends Component {
-    constructor() {
-        super();
-    }
-
+class Game extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello world</h1>
+            <div className="game">
+              <div className="game-board">
+                <Board />
+              </div>
+              <div className="game-info">
+                <div>{/* status */}</div>
+                <ol>{/* TODO */}</ol>
+              </div>
             </div>
-        )
+        );
     }
 }
 
